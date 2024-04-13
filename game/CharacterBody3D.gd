841cb5f9224cda06784c34dde26583b71f0511b6
Colby Klein <shakesoda@gmail.com>
2024-04-12 23:41:40 -0700

    add connection rod display

--- game/CharacterBody3D.gd
+++ game/CharacterBody3D.gd
@@ -129,15 +129,38 @@ func _is_connected(a: RigidBody3D, b: RigidBody3D) -> bool:
 var _connections: Dictionary = {}
 var _constraints: Array[Joint3D] = []
 
+class GlueJoint extends Generic6DOFJoint3D:
+	var display := MeshInstance3D.new()
+	func _init(a: RigidBody3D, b: RigidBody3D) -> void:
+		var cap := CapsuleMesh.new()
+		cap.height = a.global_position.distance_to(b.global_position)
+		cap.radius = 0.1
+		display.mesh = cap
+		display.rotate_x(PI/2)
+		add_child(display)
+		node_a = a.get_path()
+		node_b = b.get_path()
+
+	func _update_position():
+		var a := get_node_or_null(node_a)
+		var b := get_node_or_null(node_b)
+		if not a or not b:
+			return
+		look_at_from_position((a.global_position + b.global_position) / 2, b.global_position)
+
+	func _ready() -> void:
+		_update_position()
+
+	func _physics_process(_delta: float) -> void:
+		_update_position()
+
 func _attach(a: RigidBody3D, b: RigidBody3D):
 	var path_a := a.get_path()
 	var path_b := b.get_path()
 	if not _is_connected(a, b):
-		var constraint := Generic6DOFJoint3D.new()
+		var constraint := GlueJoint.new(a, b)
 		_connections[path_a].append(path_b)
 		_connections[path_b].append(path_a)
-		constraint.node_a = path_a
-		constraint.node_b = path_b
 		a.add_sibling(constraint)
 		_constraints.append(constraint)
 		print("attached %s and %s" % [a.name, b.name])
