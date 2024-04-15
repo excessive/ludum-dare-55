1b1b6dfce65210cd65db6d8a29ca1a34c96babae
Colby Klein <shakesoda@gmail.com>
2024-04-15 15:49:49 -0700

    fix save for continues

--- game/save_value.gd
+++ game/save_value.gd
@@ -5,7 +5,7 @@ class_name SaveValue
 @export var save_value: String
 
 func _ready() -> void:
-	if save_flag and not Globals.check_flag(save_flag):
+	if save_flag and save_value:
 		if OS.has_feature("editor"):
 			print("set ", save_flag, " = ", save_value)
 		Globals.flags[save_flag] = save_value
